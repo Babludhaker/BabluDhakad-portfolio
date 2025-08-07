c11b4880ff1357cdc8c8ffc93fd8fe00ba86b762
Bablu Dhakad <babludhaker328@gmail.com>
2025-08-07 21:40:23 +0530

    update

--- src/components/Home/Home2.jsx
+++ src/components/Home/Home2.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import myImg from "../../assets/avatar.svg";
 import profileImage from "../../assets/profileImage.jpg";
 import Tilt from "react-parallax-tilt";
 import {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/logo.png";
+import logo from "../assets/logo.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
 
-import profileImage from "../Assets/profileImage.jpg"; // Replace with your profile image import
+import profileImage from "../assets/profileImage.jpg"; // Replace with your profile image import
 import {
   AiFillStar,
   AiOutlineHome,
